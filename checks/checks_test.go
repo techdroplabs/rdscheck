f4b89ee0be4ba39c6aee3e5d9e214b3e8ef40fbd
Matthieu Dolci <matthieu.dolci@gmail.com>
2019-11-19 22:42:16 -0800

    later

--- checks/checks_test.go
+++ checks/checks_test.go
@@ -2,7 +2,6 @@ package checks
 
 import (
 	"testing"
-	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
@@ -95,55 +94,5 @@ func TestCheckRegexAgainstRow_int64False(t *testing.T) {
 	mockdb.ExpectQuery("SELECT number FROM database").WillReturnRows(rows)
 
 	value := c.CheckRegexAgainstRow("SELECT number FROM database", "^99$")
-	assert.False(t, value)
-}
-
-func TestCheckRegexAgainstRow_timeTrue(t *testing.T) {
-	db, mockdb, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	c := &Client{
-		DB: db,
-	}
-
-	time1 := time.Now()
-	string1 := time1.Format("2006-01-02T15:04:05.999999-07:00")
-	regex1 := "^" + string1 + "$"
-	time2 := time.Now().Add(time.Second * 3600)
-
-	rows := sqlmock.NewRows([]string{"time"}).
-		AddRow(time1).
-		AddRow(time2)
-
-	mockdb.ExpectQuery("SELECT time FROM database").WillReturnRows(rows)
-
-	value := c.CheckRegexAgainstRow("SELECT time FROM database", regex1)
-	assert.True(t, value)
-}
-
-func TestCheckRegexAgainstRow_timeFalse(t *testing.T) {
-	db, mockdb, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	c := &Client{
-		DB: db,
-	}
-
-	time1 := time.Now()
-	time2 := time.Now().Add(time.Second * 3600)
-
-	rows := sqlmock.NewRows([]string{"time"}).
-		AddRow(time1).
-		AddRow(time2)
-
-	mockdb.ExpectQuery("SELECT time FROM database").WillReturnRows(rows)
-
-	value := c.CheckRegexAgainstRow("SELECT time FROM database", "^2006-01-02T15:04:05.999999-07:00$")
 	assert.False(t, value)
 }
